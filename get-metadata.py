143ee95e59a10c1a873e499d7dafabf796340421
Lukas Benedix <benedix@zedat.fu-berlin.de>
2014-10-23 23:05:27 +0200

    added collection date added hacky csv output

--- get-metadata.py
+++ get-metadata.py
@@ -1,5 +1,6 @@
 import json, io, re, requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 
 def get_datasets(url):
@@ -12,7 +13,7 @@ def get_datasets(url):
         r = requests.get(url.format(page))
         soup = BeautifulSoup(r.text)
         for link in soup.select('h2 a'):
-            yield link['href']
+            yield (link['href'], link.text)
 
 
 def get_metadata(url):
@@ -21,6 +22,7 @@ def get_metadata(url):
 
     metadata = dict()
     metadata['_url'] = url.format(d)
+    metadata['_collection_date'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     for elem in soup.select('.datasetview_container .datasetview_row'):
         for field in elem.select('.field'):
@@ -44,19 +46,39 @@ if __name__ == '__main__':
     datasets_url = 'http://daten.berlin.de/datensaetze?page={}'
     documents_url = 'http://daten.berlin.de/dokumente?page={}'
 
+    all_labels = set()
     all_metadata = list()
-    for d in get_datasets(datasets_url):
+
+    # iterate over all dataset urls
+    for d, t in get_datasets(datasets_url):
         m = get_metadata(base_url.format(d))
         m['_type'] = 'dataset'
+        m['_title'] = t
         all_metadata.append(m)
-        print( m )
+        for k in m.keys(): all_labels.add(k)
+        print(json.dumps(m, sort_keys=1, ensure_ascii=False))
 
-    for d in get_datasets(documents_url):
+    # iterate over all document urls
+    for d, t in get_datasets(documents_url):
         m = get_metadata(base_url.format(d))
         m['_type'] = 'document'
+        m['_title'] = t
         all_metadata.append(m)
+        for k in m.keys(): all_labels.add(k)
+        print(json.dumps(m, sort_keys=1, ensure_ascii=False))
 
+    # write json file
     with io.open('daten-berlin_metadata.json', 'w', encoding='utf8') as json_file:
-        json_file.write(unicode(json.dumps(all_metadata, indent=2, sort_keys=True, ensure_ascii=False)))
+        json_file.write((json.dumps(all_metadata, indent=2, sort_keys=True, ensure_ascii=False)))
 
-    print(json.dumps(all_metadata, indent=2, sort_keys=True))
+    # write csv
+    with io.open('daten-berlin_metadata.csv', 'w', encoding='utf8') as csv_file:
+        for l in sorted(all_labels): csv_file.write(l + ';')
+        csv_file.write('\n')
+        for m in all_metadata:
+            for l in sorted(all_labels):
+                if l in m:
+                    csv_file.write(str(m[l]) + ';')
+                else:
+                    csv_file.write(';')
+            csv_file.write('\n')
